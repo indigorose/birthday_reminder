d8e0c3ed4d61530f33de37d68d898c0414fc4d9b
indigorose <indigorose049@gmail.com>
2022-09-12 05:23:28 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import ErrorExample from "./tutorial/1-useState/setup/1-error-example";
+import Setup from "./useState";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>5 Birthdays Today</h1>
+      {/* find our how to add the count of data to the h1 title */}
+      <Setup />
     </div>
   );
 }
-
 export default App;
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const data = [
+  { id: 1, name: "Alice", age: 20 },
+  { id: 1, name: "John", age: 68 },
+  { id: 1, name: "Martin", age: 53 },
+  { id: 1, name: "Lois", age: 45 },
+  { id: 1, name: "Oscar", age: 36 },
+];
+// add image details to the data - use the random image generator in the python notes.
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
